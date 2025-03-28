e7939fd106e3c5723104068d5918000e369cd73d
badbedbett <134656318+badbedbett@users.noreply.github.com>
2025-03-28 23:52:02 +0300

    Create task-list-component.js

---
+++ src/view/task-list-component.js
@@ -0,0 +1,31 @@
+import {createElement} from '../framework/render.js';
+
+function createTaskListComponentTemplate(title) {
+  return (
+    `<div class="column">
+      <h2>${title}</h2>
+    </div>`
+  );
+}
+
+export default class TaskListComponent {
+  constructor(title) {
+    this.title = title;
+  }
+
+  getTemplate() {
+    return createTaskListComponentTemplate(this.title);
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
